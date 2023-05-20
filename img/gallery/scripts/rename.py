3f727a998adcc403985d5dfb9d70b3fc66fd2f4d
Sarath-Ju <58463208+Sarath-Ju@users.noreply.github.com>
2023-05-20 16:30:02 +0100

    [ADD] new pics

--- img/gallery/scripts/rename.py
+++ img/gallery/scripts/rename.py
@@ -1,11 +1,11 @@
 import os
 
-dir = 'new_products/'
-count =97
+dir = 'products/'
+count =0
 for file in os.listdir(dir):
     count +=1
     old_filepath = os.path.join(dir, file)
-    new_name = 'bsramanan_wedding_' + str(count) +'.jpg'
+    
     new_filepath = os.path.join(dir, new_name)
     # if file startwith
 
